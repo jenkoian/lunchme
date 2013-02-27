e3aa5e4a261402de798a0ad8285508feeaae91ba
Ian Jenkins <ian@jenkins-web.co.uk>
2013-02-27 20:24:47 +0000

    Venue CRUB and index.html

--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/jsonapi'
 require 'sinatra/mongo'
+require 'json'
 
 set :mongo, 'mongodb://tobias:tobias@linus.mongohq.com:10038/app12523429'
 
@@ -10,9 +11,29 @@ venue = {
 	:url => "",
 	:rating => 0
 }
-
 #puts mongo["venue"].insert venue
 
-get '/' do
-   api_response mongo["venue"].find_one
+get '/', :provides => 'html' do
+   erb :index
+end
+
+get '/venues/' do
+   api_response mongo["venue"].find.to_a
+end
+
+get '/venue/:id' do
+   api_response mongo["venue"].find("_id" => BSON::ObjectId(params[:id])).to_a
+end
+
+post '/venue/' do
+   api_response mongo["venue"].insert JSON.parse(request.body.read.to_s)
+end
+
+put '/venue/:id' do
+   mongo["venue"].update({"_id" => BSON::ObjectId(params[:id])}, JSON.parse(request.body.read.to_s))
+   api_response mongo["venue"].find("_id" => BSON::ObjectId(params[:id])).to_a
+end
+
+delete '/venue/:id' do
+   api_response mongo["venue"].remove("_id" => BSON::ObjectId(params[:id]))
 end

ef27dfd534edc806dc843584bad1e240692e96c3
Ian Jenkins <ian@jenkins-web.co.uk>
2013-02-27 19:38:23 +0000

    Second commit

--- web.rb
+++ web.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sinatra/jsonapi'
+require 'sinatra/mongo'
+
+set :mongo, 'mongodb://tobias:tobias@linus.mongohq.com:10038/app12523429'
+
+venue = {
+	:name => "CityArms",
+	:address => "Cardiff", 
+	:url => "",
+	:rating => 0
+}
+
+#puts mongo["venue"].insert venue
 
 get '/' do
-   "Hello World!"
+   api_response mongo["venue"].find_one
 end
